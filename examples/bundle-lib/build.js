5591c17348ce467428d233013604c7c22a6b078d
mycoin <nqliujiangtao@gmail.com>
2021-06-17 22:33:39 +0800

    update

--- examples/bundle-lib/build.js
+++ examples/bundle-lib/build.js
@@ -1,11 +1,16 @@
 const { webpackFactory, webpack } = require('../..')
 
 const webpackConfig = webpackFactory({
-  extractCSS: false,
+  extractCSS: true,
 })
 
-webpack(webpackConfig).run((error, stats) => {
+const handler = (error, stats) => {
   console.log(stats.toString({
     colors: true,
   }))
-})
+}
+const watchOption = {
+  aggregateTimeout: 500,
+}
+
+webpack(webpackConfig).watch(watchOption, handler)
--- examples/bundle-lib/src/index.js
+++ examples/bundle-lib/src/index.js
@@ -1,13 +1,16 @@
-import isObject from 'is-object'
-import isNumber from 'is-number'
+// import isObject from 'is-object'
+// import isNumber from 'is-number'
+
+// import render from './index.ejs'
 
 import './index.scss'
+// import './index.less'
 
-const fn = () => {
-  document.body.innerHTML = '阿里巴巴'
-  if (isObject(document)) {
-    isNumber(document)
-  }
-}
+// const fn = () => {
+//   document.body.innerHTML = '阿里巴巴' + render({})
+//   if (isObject(document)) {
+//     console.error(isNumber(document))
+//   }
+// }
 
-document.body = fn()
+// fn()
